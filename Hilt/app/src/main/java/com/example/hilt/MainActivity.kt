9f7bd4ff645902f4036ada1da73ffdad4f17b337
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-14 12:12:06 +0530

    field injection - activity level

--- Hilt/app/src/main/java/com/example/hilt/MainActivity.kt
+++ Hilt/app/src/main/java/com/example/hilt/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.hilt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var userRepository: UserRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userRepository.saveUser()
     }
 }
--- Hilt/app/src/main/java/com/example/hilt/UserApplication.kt
+++ Hilt/app/src/main/java/com/example/hilt/UserApplication.kt
@@ -2,16 +2,11 @@ package com.example.hilt
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
 class UserApplication : Application() {
 
-    @Inject
-    lateinit var userRepository: UserRepository
     override fun onCreate() {
         super.onCreate()
-
-        userRepository.saveUser()
     }
 }
