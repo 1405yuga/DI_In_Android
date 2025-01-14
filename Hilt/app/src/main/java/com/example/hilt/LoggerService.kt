76677cc8a11482354bba7a6cd53e8cf456869b49
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-14 19:22:42 +0530

    making scoped instances

--- Hilt/app/src/main/java/com/example/hilt/LoggerService.kt
+++ Hilt/app/src/main/java/com/example/hilt/LoggerService.kt
@@ -1,8 +1,11 @@
 package com.example.hilt
 
 import android.util.Log
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@ActivityScoped
 class LoggerService @Inject constructor() {
     val TAG = this.javaClass.simpleName
 
--- Hilt/app/src/main/java/com/example/hilt/MainActivity.kt
+++ Hilt/app/src/main/java/com/example/hilt/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.hilt
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    val TAG = this.javaClass.simpleName
+
+    @Inject
+    lateinit var loggerService1: LoggerService
+
+    @Inject
+    lateinit var loggerService2: LoggerService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "logger 1 : ${loggerService1.hashCode()}")
+        Log.d(TAG, "logger 2 : ${loggerService2.hashCode()}")
     }
 }
