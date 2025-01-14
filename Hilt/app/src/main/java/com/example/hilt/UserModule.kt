4e3455caa60c56a167b28993e1b04ddfb8927120
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-14 17:34:49 +0530

    binds implementation

--- Hilt/app/src/main/java/com/example/hilt/UserModule.kt
+++ Hilt/app/src/main/java/com/example/hilt/UserModule.kt
@@ -1,16 +1,19 @@
 package com.example.hilt
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 
 @Module
 @InstallIn(ActivityComponent::class)
-class UserModule {
+abstract class UserModule {
 
-    @Provides
-    fun providesUserRepository(): UserRepository {
-        return FirebaseRepository()
-    }
+//    @Provides
+//    fun providesUserRepository(): UserRepository {
+//        return FirebaseRepository()
+//    }
+
+    @Binds
+    abstract fun providesUserRepository(sqlRepository: SQLRepository): UserRepository
 }
