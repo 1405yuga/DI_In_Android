284e728d19605dd2330a5dbf335eb4c3ae14e841
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-15 12:23:33 +0530

    added show-notification inside work manager

--- WorkManager/app/src/main/java/com/example/workmanager/MyWorker.kt
+++ WorkManager/app/src/main/java/com/example/workmanager/MyWorker.kt
@@ -7,10 +7,13 @@ import androidx.work.WorkerParameters
 class MyWorker(context: Context, workerParameters: WorkerParameters) :
     Worker(context, workerParameters) {
     override fun doWork(): Result {
+        val notificationHelper = NotificationHelper(applicationContext)
+        notificationHelper.createNotificationChannel()
 
         try {
             val inputData = inputData.getString("INPUT_KEY")
-            Thread.sleep(5000)
+            Thread.sleep(3000)
+            notificationHelper.showNotification("MyWorker notification", inputData.toString())
             println("Received input : $inputData")
         } catch (e: Exception) {
             e.printStackTrace()
