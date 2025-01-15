27ec724849f07e7e42baa3557fd7bc1da1c0780a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-15 11:33:48 +0530

    added constraints on workRequest

--- WorkManager/app/src/main/java/com/example/workmanager/MainActivity.kt
+++ WorkManager/app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.workmanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
 import androidx.work.Data
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
@@ -15,10 +17,15 @@ class MainActivity : AppCompatActivity() {
         val someInput = Data.Builder()
             .putString("INPUT_KEY", "Hello there!")
             .build()
-
+//        define constraints
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
 //        create request
         val workerRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
             .setInputData(someInput)
+            .setConstraints(constraints)
             .build()
 
         //enqueue the work
