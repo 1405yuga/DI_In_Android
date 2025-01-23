71c958f016a2de53ee88e829826ad9a5e2d25afa
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-01-23 17:23:28 +0530

    implemented loops

--- KotlinPlayGround/src/main/kotlin/Main.kt
+++ KotlinPlayGround/src/main/kotlin/Main.kt
@@ -4,9 +4,11 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     whenConditions()
+    loops()
 }
 
 fun whenConditions() {
+    println("whenConditions--------------")
     val x = 3
     when (x) {
         1 -> println("x is 1")
@@ -17,13 +19,25 @@ fun whenConditions() {
 
     //when as expression and without subject
     val result = when {
-        (x > 5) -> println("x is greater than 5")
-        else -> println("x less than equal to 5")
+        (x > 5) -> "x is greater than 5"
+        else -> "x less than equal to 5"
     }
-    println(result)
+    println("result: $result")
 }
 
 fun loops() {
+    println("Loops--------------")
     val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
+    for (i in nums) print(i)
+    println()
+    for (i in nums.indices) print(nums[i])
+    println()
+    for (i in 5..nums.lastIndex) print(nums[i])
+    println()
+    var x = 3
+    while (x > 0) {
+        print(x)
+        x--
+    }
 }
